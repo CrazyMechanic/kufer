49ddfa32cbb475c3c32c7bb2f6991a99a2dd806f
CrazyMechanic <egortimchenko94@gmail.com>
2023-08-12 15:52:06 +0300

    create form container

--- src/app/components/forms/forms.module.ts
+++ src/app/components/forms/forms.module.ts
@@ -4,16 +4,19 @@ import {CommonModule} from '@angular/common';
 import {FormsRoutingModule} from './forms-routing.module';
 import {SignUpComponent} from './sign-up/sign-up.component';
 import {LoginComponent} from './login/login.component';
+import {FormContainerComponent} from "./form-container/form-container.component";
 
 
 @NgModule({
   declarations: [
     SignUpComponent,
     LoginComponent,
+    FormContainerComponent,
   ],
   exports: [
     SignUpComponent,
     LoginComponent,
+    FormContainerComponent,
   ],
   imports: [
     CommonModule,
