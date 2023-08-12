409aea26a363339248ebd768952ede67dd371b3d
CrazyMechanic <egortimchenko94@gmail.com>
2023-08-12 15:42:31 +0300

    create login component

--- src/app/components/forms/forms.module.ts
+++ src/app/components/forms/forms.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { FormsRoutingModule } from './forms-routing.module';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import {FormsRoutingModule} from './forms-routing.module';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {LoginComponent} from './login/login.component';
 
 
 @NgModule({
   declarations: [
     SignUpComponent,
+    LoginComponent,
   ],
   exports: [
     SignUpComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
