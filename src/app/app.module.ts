683b2b01be79992ec3358c4507c7105f73db567d
CrazyMechanic <egortimchenko94@gmail.com>
2023-09-02 01:14:49 +0300

    add describe block and photo block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule} from './components/forms/forms.module';
+import { CreateAnItemComponent } from './components/create-an-item/create-an-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreateAnItemComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/create-an-item/create-an-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-an-item',
+  templateUrl: './create-an-item.component.html',
+  styleUrls: ['./create-an-item.component.scss']
+})
+export class CreateAnItemComponent {
+
+}
