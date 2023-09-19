1892961c2cf8d03dee342be988972baa619fe5d6
CrazyMechanic <egortimchenko94@gmail.com>
2023-09-19 23:57:44 +0300

    end profile editing and todo opening component

--- src/app/components/profile-editing/profile-editing.component.ts
+++ src/app/components/profile-editing/profile-editing.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FORM_LABEL } from '../../data/form-data';
 
 @Component({
   selector: 'app-profile-editing',
   templateUrl: './profile-editing.component.html',
-  styleUrls: ['./profile-editing.component.scss']
+  styleUrls: ['./profile-editing.component.scss'],
 })
 export class ProfileEditingComponent {
 
+  label = FORM_LABEL;
+  // labelVisibility: Record<string, boolean> = {
+  //   name: false,
+  //   lastName: false,
+  //   email: false,
+  //   phone: false,
+  //   password: false,
+  // };
 }
