fd266c7f3121f4016de1f1494e874b2f17fd1219
CrazyMechanic <egortimchenko94@gmail.com>
2023-10-09 16:22:03 +0300

    add opening first element from home component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
-
+//   export class ProductCardComponent implements OnInit {
+//   productId: number;
+//   productData: any; // Заменить на интерфейс или модель данных
+//
+//   constructor(
+//     private route: ActivatedRoute,
+//     private productService: ProductService
+//   ) {}
+//
+//   ngOnInit(): void {
+//     this.route.params.subscribe(params => {
+//       this.productId = +params['id'];
+//       this.loadProductData(this.productId);
+//     });
+//   }
+//
+//   loadProductData(productId: number): void {
+//     // Использывать сервис productService для загрузки данных о продукте по ID
+//     this.productData = this.productService.getProduct(productId);
+//   }
+// }
 }
